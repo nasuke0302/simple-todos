eb9f67adbdaa3581ad6c27df8080c5b6cd54c68c
Alberto Esquivel <albertoesquivelanias@gmail.com>
2019-10-16 18:56:25 -0400

    added accounts-ui

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,19 +1,54 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 
 import { Tasks } from '../api/tasks';
 import Task from './components/Task.jsx';
+import AccountsUIWrapper from './components/AccountsUIWrapper.jsx';
 
 const App = props => {
 
+    const textInput = useRef('textInput');
+    const [hideCompleted, setHideCompleted] = useState(false);
+
     const renderTasks = () => {
-        return props.tasks.map(task => <Task key={task._id} task={task} />);
+        let filteredTask = props.tasks;
+        if (hideCompleted) {
+            filteredTask = filteredTask.filter(task => !task.checked);
+        }
+        return filteredTask.map(task => <Task key={task._id} task={task} />);
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        Tasks.insert({ text: textInput.current.value, createdAt: new Date() })
+
+        textInput.current.value = '';
     }
 
     return (
         <div className="container">
             <header>
-                <h1>Todo List</h1>
+                <h1>Todo List {props.incompleteTasks}</h1>
+                <label className="hide-completed">
+                    <input
+                        type='checkbox'
+                        readOnly
+                        checked={hideCompleted}
+                        onClick={() => setHideCompleted(!hideCompleted)}
+                    />
+                    Hide Completed Task
+                </label>
+
+                <AccountsUIWrapper />
+
+                <form className='new-task' onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        ref={textInput}
+                        placeholder='Type to add new tasks'
+                    />
+                </form>
             </header>
 
             <ul>
@@ -25,6 +60,7 @@ const App = props => {
 
 export default withTracker(() => {
     return {
-        tasks: Tasks.find({}).fetch(),
+        tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+        incompleteTasks: Tasks.find({ checked: { $ne: true } }).count()
     };
 })(App);
--- imports/ui/components/Task.jsx
+++ imports/ui/components/Task.jsx
@@ -1,5 +1,34 @@
 import React from 'react'
+import { Tasks } from '../../api/tasks';
 
 export default props => {
-    return <li>{props.task.text}</li>
+    
+    const taskClassName = props.task.checked ? 'checked' : '';
+    
+    const deleteThisTask = () => {
+        Tasks.remove({ _id: props.task._id })
+    }
+    
+    const toggleChecked = () => {
+        Tasks.update({ _id: props.task._id }, { $set: { checked: !props.task.checked } });
+    }
+
+    return (
+        <li className={taskClassName} >
+            <button
+                className="delete"
+                onClick={deleteThisTask}
+            >
+                &times;
+            </button>
+
+            <input
+                type="checkbox"
+                readOnly
+                checked={props.task.checked}
+                onClick={toggleChecked}
+            />
+            <span>{props.task.text}</span>
+        </li >
+    )
 }
